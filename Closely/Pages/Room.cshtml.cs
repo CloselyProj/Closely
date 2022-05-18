863a01f9c107a8ad3923fed4d7fbe9a06ea31b37
margo <margoshka.ri6@gmail.com>
2022-05-18 21:00:26 +0300

    Add project files.

---
+++ Closely/Pages/Room.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Closely.Pages
+{
+    public class RoomModel : PageModel
+    {
+        public string Message = string.Empty;
+        public void OnPost(string movie_link)
+        {
+            if(movie_link.Contains("watch?v="))
+            {
+                Message = movie_link.Replace("watch?v=", "embed/");
+            }
+        }
+    }
+}
